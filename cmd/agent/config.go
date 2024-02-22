83757305ce08d420bf2d3be941bde111e79f6ea4
Vladimir Denisov <code@denisov.org>
2024-02-22 09:23:41 +0300

    add poll&report interval checks, poll<>rep reorder

--- cmd/agent/config.go
+++ cmd/agent/config.go
@@ -18,29 +18,37 @@ func InitAgentConfig() AgentConfig {
 
 	//set defaults and read command line
 	flag.StringVar(&cf.Endpoint, "a", "localhost:8080", "the address:port server endpoint to send metric data")
-	flag.Int64Var(&cf.ReportInterval, "r", 10, "metric reporting frequency in seconds")
 	flag.Int64Var(&cf.PollInterval, "p", 2, "metric poll interval in seconds")
+	flag.Int64Var(&cf.ReportInterval, "r", 10, "metric reporting frequency in seconds")
 	flag.Parse()
 
 	//parse env variables
 	if val, found := os.LookupEnv("ADDRESS"); found {
 		cf.Endpoint = val
 	}
-	if val, found := os.LookupEnv("REPORT_INTERVAL"); found && (val != "") {
-		intval, err := strconv.ParseInt(val, 10, 64)
-		if err == nil {
-			cf.ReportInterval = intval
-		}
-	}
-	if val, found := os.LookupEnv("POLL_INTERVAL"); found && (val != "") {
+	if val, found := os.LookupEnv("POLL_INTERVAL"); found {
 		intval, err := strconv.ParseInt(val, 10, 64)
 		if err == nil {
 			cf.PollInterval = intval
 		}
 	}
+	if val, found := os.LookupEnv("REPORT_INTERVAL"); found {
+		intval, err := strconv.ParseInt(val, 10, 64)
+		if err == nil {
+			cf.ReportInterval = intval
+		}
+	}
 
 	if cf.Endpoint == "" {
 		panic("PANIC: endpoint address:port is not set")
+	}
+
+	if cf.PollInterval == 0 {
+		panic("PANIC: poll interval is not set")
+	}
+
+	if cf.ReportInterval == 0 {
+		panic("PANIC: report interval is not set")
 	}
 
 	// // Access and print non-flag arguments
