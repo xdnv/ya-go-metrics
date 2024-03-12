3c57e103f2e5f9e52f63e4205b61ee69b6e136bf
Vladimir Denisov <code@denisov.org>
2024-03-12 23:51:18 +0300

    add SQL ping

--- cmd/server/handle_ping.go
+++ cmd/server/handle_ping.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -12,12 +14,19 @@ import (
 func pingDBServer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
-	conn, err := sql.Open("pgx", sc.DatabaseDSN)
+	db, err := sql.Open("pgx", sc.DatabaseDSN)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("error connecting to DB server: %s", err), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("error opening DB connection: %s", err), http.StatusInternalServerError)
 		return
 	}
-	defer conn.Close()
+	defer db.Close()
+
+	dbctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err = db.PingContext(dbctx); err != nil {
+		http.Error(w, fmt.Sprintf("error pinging DB server: %s", err), http.StatusInternalServerError)
+		return
+	}
 
 	body := "Ping OK"
 	_, _ = w.Write([]byte(body))
